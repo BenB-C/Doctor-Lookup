a1bbb36e08a32e3c3420ddb89fab00d8b7a84db3
bcauchon <bcauchon@gmail.com>
2019-07-05 10:54:35 -0700

    Add implementation of getDoctorsByIssue

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,8 +1,19 @@
 export default class DoctorLookup {
-  constructor(){
-
-  }
   // Returns a list of doctors who treat the given medical issue
   getDoctorsByIssue(issue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
