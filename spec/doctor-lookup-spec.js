ad62888e7eb7f64bd8d2c91a45a0ba9abf8b03f4
bcauchon <bcauchon@gmail.com>
2019-07-05 11:04:54 -0700

    Add two more doctor names to test

--- spec/doctor-lookup-spec.js
+++ spec/doctor-lookup-spec.js
@@ -5,11 +5,19 @@ const doctorLookup = new DoctorLookup();
 describe('getDoctorsByIssue', function() {
   it('should return a list of doctors who treat sore throats', function() {
     doctorLookup.getDoctorsByIssue("sore throat").then((response) => {
-      const body = JSON.parse(response);
-      const first_name = body.data[0].profile.first_name;
-      const middle_name = body.data[0].profile.middle_name;
-      const last_name = body.data[0].profile.last_name;
-      expect(`${first_name} ${middle_name} ${last_name}`).toEqual("John William Topping");
+      let body = JSON.parse(response);
+      let doctorNames = []
+      for (let i = 0; i < 3; i++) {
+        let first_name = body.data[i].profile.first_name;
+        let middle_name = body.data[i].profile.middle_name;
+        let last_name = body.data[i].profile.last_name;
+        doctorNames.push(`${first_name} ${middle_name} ${last_name}`);
+      }
+      expect(doctorNames[0]).toEqual("John William Topping");
+      expect(doctorNames[1]).toEqual("Craig Kevin Hertler");
+      expect(doctorNames[2]).toEqual("David Franklin Wilson");
+
+
     });
   });
 });
