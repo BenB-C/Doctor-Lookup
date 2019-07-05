bb659e4f2fd02d825bda39b97eb2f3e2aef0bf4e
bcauchon <bcauchon@gmail.com>
2019-07-05 11:04:25 -0700

    Add missing semicolon

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -3,7 +3,6 @@ export default class DoctorLookup {
   getDoctorsByIssue(issue) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
@@ -11,7 +10,7 @@ export default class DoctorLookup {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
