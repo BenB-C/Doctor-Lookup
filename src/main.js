3ea440ecdbc558509416d45ab9c0576403de3c61
bcauchon <bcauchon@gmail.com>
2019-07-05 12:21:38 -0700

    Add html and js for getting user input and api call response

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DoctorLookup from './../src/doctor-lookup.js';
 
-$(document).ready(function (){
+const doctorLookup = new DoctorLookup();
+
+$(document).ready(() => {
+  $("form#doctor-search").submit((event) => {
+    event.preventDefault();
+    let searchType = $("select#search-type").val();
+    let query = $("input#query").val();
+    let getDoctorsPromise = (searchType === "name" ? doctorLookup.getDoctorsByName : doctorLookup.getDoctorsByIssue);
+
+    getDoctorsPromise(query).then((response) => {
+      let doctors = JSON.parse(response).data;
+      console.log(doctors);
+    });
+  });
 });
